d842d4bea7ead4f282fab8751a13e4e5df5cfda4
Emil Holmstedt <holmstedt.emil@gmail.com>
2019-08-04 02:46:23 +0200

    Create functions.js

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+function dropdownContentShow() {
+  document.getElementById("dropdown").classList.toggle("toggle");
+}
+
+function openCollapsedNav() {
+  document.getElementById("collapsed-nav").classList.toggle("show-nav");
+}
